81e120bc83e11fdf3bb4f39f13b9235553226461
lpelczar <lukepelczar@gmail.com>
2017-11-03 18:25:49 +0100

    Refactor user input for adding item

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from view import MenuView, AddItemView
 from model import Model
+import os
 
 
 class Controller:
@@ -18,27 +19,39 @@ class Controller:
         self.add_item_view = AddItemView()
 
     def begin(self):
+        os.system('clear')
         self.show_menu()
         while True:
             option = input('Choose option: ')
             if option in self.OPTIONS.keys():
                 if option == '1':
                     self.add_todo_item()
-                    break
+                if option == '2':
+                    self.modify_item()
 
     def show_menu(self):
         self.menu_view.display()
 
     def add_todo_item(self):
+        name = self.ask_name_input()
+        description = self.ask_description_input()
+        self.model.add_item(name, description)
+        self.add_item_view.display(name)
+
+    @staticmethod
+    def ask_name_input():
         while True:
             name = input('Enter name (max 20 characters): ')
             if len(name) > 20:
                 print('Name is too long!')
-            break
+            else:
+                return name
+
+    @staticmethod
+    def ask_description_input():
         while True:
             description = input('Enter description (max 150 characters): ')
             if len(description) > 150:
                 print('Description is too long!')
-            break
-        self.model.add_item(name, description)
-        self.add_item_view.display(name)
+            else:
+                return description
