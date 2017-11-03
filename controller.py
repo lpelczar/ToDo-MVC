4bc3df0be2cb4053a89c55fff48662a5dcaf18dd
lpelczar <lukepelczar@gmail.com>
2017-11-03 17:35:56 +0100

    Implement printing menu from controller

--- controller.py
+++ controller.py
@@ -1,4 +1,12 @@
+from view import *
+from model import Model
+
+
 class Controller:
 
     def __init__(self):
-        ...
+        self.model = Model()
+        self.menuview = MenuView()
+
+    def show_menu(self):
+        self.menuview.display_menu()
--- view.py
+++ view.py
@@ -1,5 +1,21 @@
 class MenuView:
-    ...
+
+    OPTIONS = {'1': 'Add Todo Item',
+               '2': 'Modify Item',
+               '3': 'Delete Item',
+               '4': 'Mark Item',
+               '5': 'Display Items',
+               '6': 'Display Specific Item',
+               '7': 'Exit program'}
+
+    def __init__(self):
+        ...
+
+    def display_menu(self):
+        print('Todo App. What do you want to do?')
+        for k, v in self.OPTIONS.items():
+            print(' {}. {}'.format(k, v))
+        print('Enter option: ')
 
 
 class AddItemView:
