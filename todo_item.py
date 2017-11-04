b2095e04db68b3d014ca265d32bd8eb33f383859
lpelczar <lukepelczar@gmail.com>
2017-11-04 12:19:39 +0100

    Add docstrings to todo item

--- todo_item.py
+++ todo_item.py
@@ -1,5 +1,13 @@
 class TodoItem:
+    """
+    Class TodoItem represents a single todo item
 
+    Attributes:
+    :param name: String -> name of the task
+    :param description: String -> description of the task
+    :param deadline: Datetime object -> deadline of the task, default: None
+    :param is_done: bool -> True if task is done else False, default: False
+    """
     def __init__(self, name, description, deadline=None, is_done=False):
         self.name = name
         self.description = description
@@ -11,5 +19,8 @@ class TodoItem:
         self.deadline = None
 
     def __str__(self):
+        """
+        Return string representation of a task
+        """
         mark = 'X' if self.is_done else ' '
         return '[{}] {}'.format(mark, self.name)
